a70c6d25dd114586cf0e681d6fcf24fd2ccf5399
jaloliddinshamsiddinov <125132177+jaloliddinshamsiddinov@users.noreply.github.com>
2023-09-25 16:43:59 +0500

    fix/tab

--- js/app.js
+++ js/app.js
@@ -1,10 +1,60 @@
-import { getSlideData } from "./getdata.js";
-const hero = document.querySelector(".hero")
+import { getProductsData, getCategoryProduct } from "./getdata.js";
+const tab_list = document.querySelector(".tab-list");
+const tab_box = document.querySelector(".tab_box");
 
-const slide = async () => {
-  const data = await getSlideData();
-//   hero.style.backgroundImage = data[0].img
-hero.style.backgroundImage = data[0].img;
-  console.log(data[0].img);
+const renderProduct = async (id) => {
+  const data = await getCategoryProduct(id);
+  tab_box.innerHTML = data
+    .map(
+      (item) => `
+  <div class="tab_box-item">
+  <div class="tab-img">
+    <img src="${item.img}" alt="img">
+    <div class="tab-item_hover">
+        <div class="icon"><i class="fa-regular fa-heart" style="color: #33a0ff;"></i></div>
+        <div class="icon"><i class="fa-solid fa-basket-shopping" style="color: #33a0ff;"></i></div>
+    </div>
+  </div>
+  <div class="tab-description">
+    <h3 class="nikePrise">${item.title}</h3>
+    <div class="tabStars">
+        <i class="fa-solid fa-star" style="color: #e7ff2e;"></i>
+        <i class="fa-solid fa-star" style="color: #e7ff2e;"></i>
+        <i class="fa-solid fa-star" style="color: #e7ff2e;"></i>
+        <i class="fa-solid fa-star" style="color: #e7ff2e;"></i>
+        <i class="fa-solid fa-star" style="color: #a8a8a8;"></i>
+    </div>
+    <div class="tab_price">
+        <h3>$299,43</h3>
+        <p class="text-decoration-line-through">$534,33</p>
+        <h4>24% Off</h4>
+    </div>
+  </div>
+</div>
+  `
+    )
+    .join("");
 };
-slide();
+
+const renderTab = async () => {
+  const data = await getProductsData();
+  tab_list.innerHTML = data
+    .map(
+      (item, index) =>
+        `<li data-product="${item.id}" class="tab-item ${
+          index == 0 ? "active" : ""
+        }">${item.title}</li>`
+    )
+    .join("");
+  renderProduct(data[0]?.id);
+};
+renderTab();
+
+tab_list.addEventListener("click", (e) => {
+  const lis = document.querySelectorAll(".tab-list .tab-item");
+  for (let i of lis) {
+    i.classList.remove("active");
+  }
+  e.target.classList.add("active");
+  renderProduct(e.target.dataset.product)
+});
--- js/getdata.js
+++ js/getdata.js
@@ -1,10 +1,20 @@
-const baseUrl = "http://localhost:3000/slide";
+const baseUrl = "http://localhost:3000/category";
 
-export async function getSlideData(params) {
+export async function getProductsData() {
   try {
     const res = await fetch(baseUrl);
     const data = await res.json();
 
-    return data?.map((item) => ({ img: item.img }));
+    return data?.map((item) => ({ title: item.name, id: item.id}));
   } catch (error) {}
+}
+
+export async function getCategoryProduct(id) {
+  try {
+    const response = await fetch(`${baseUrl}/${id}`)
+    const data = await response.json()
+    return data?.product
+  } catch (error) {
+    
+  }
 }
